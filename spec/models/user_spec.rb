2c5a9ad7bda3b275d7262c93ade37b961ae47049
r4o <r4o7r9@gmail.com>
2020-04-08 17:40:09 +0300

    Lesson_12_6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context 'user already has authorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+  end
 end
