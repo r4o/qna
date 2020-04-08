1bc767d7fb254d4311d4f63e3d90bde3293a951d
r4o <r4o7r9@gmail.com>
2020-04-08 13:01:54 +0300

    Lesson13_3

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -14,6 +14,23 @@ describe 'Ability' do
   end
 
   describe 'for admin' do
-    
+    let(:user) { create :user, admin: true }
+
+    it { should be_able_to :manage, :all }
+  end
+
+  describe 'for user' do
+    let(:user) { create :user }
+    let(:other) { create :user}
+
+    it { should_not be_able_to :manage, :all }
+    it { should be_able_to :read, :all }
+
+    it { should be_able_to :create, Question }
+    it { should be_able_to :create, Answer }
+    it { should be_able_to :create, Comment }
+
+    it { should be_able_to :update, create(:question, user: user), user: user}
+    it { should_not be_able_to :update, create(:question, user: other), user: user}
   end
 end
