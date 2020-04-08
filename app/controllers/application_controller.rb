3f906bfa72b926539b833551c9963fb7cafc8d58
r4o <r4o7r9@gmail.com>
2020-04-08 11:46:35 +0300

    Lesson13_2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
 end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Ability' do
+  subject(:ability) { Ability.new(user) }
+
+  describe 'for guest' do
+    let(:user) { nil }
+
+    it { should be_able_to :read, Question }
+    it { should be_able_to :read, Answer }
+    it { should be_able_to :read, Comment }
+
+    it { should_not be_able_to :manage, :all }
+  end
+
+  describe 'for admin' do
+    
+  end
+end
