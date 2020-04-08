436a9ce475e7dd93b6a2d359dcc39ebeca3015bf
r4o <r4o7r9@gmail.com>
2020-04-08 13:54:50 +0300

    Lesson_13_Refactoring_2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,6 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, alert: exception.message
   end
+
+  check_authorization
 end
