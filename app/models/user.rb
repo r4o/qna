a24c2d303510214ad5485929be343de5ce89cc35
r4o <r4o7r9@gmail.com>
2020-04-08 20:56:05 +0300

    Lesson_12_Refactoring!

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,17 @@ class User < ApplicationRecord
     email = auth.info[:email]
     user = User.where(email: email).first
     if user
-      user.authorizations.create(provider: auth.provider, uid: auth.uid)
+      user.create_authorization(auth)
     else
       password = Devise.friendly_token[0, 20]
       user = User.create!(email: email, password: password, password_confirmation: password)
-      user.authorizations.create(provider: auth.provider, uid: auth.uid)
+      user.create_authorization(auth)
     end
+
     user
+  end
+
+  def create_authorization(auth)
+    self.authorizations.create(provider: auth.provider, uid: auth.uid)
   end
 end
