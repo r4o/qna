e2940eaa2b11222e3420dc484a6dd32a384418e5
r4o <r4o7r9@gmail.com>
2020-04-08 17:47:45 +0300

    Lesson_12_7

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+
+  has_many :authorizations
+  
 end
