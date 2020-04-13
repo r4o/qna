c1cd4cb31101587b0f4005fc04f2cb264cb02053
r4o <r4o7r9@gmail.com>
2020-04-13 12:54:24 +0300

    Lesson_15_9

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -17,6 +17,8 @@ describe 'Questions API' do
     context 'authorized' do
       let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
+      let(:question) { questions.first }
+      let!(:answer) { create(:answer, question: question) }
 
       before { get '/api/v1/questions', format: :json, access_token: access_token.token }
 
@@ -30,8 +32,19 @@ describe 'Questions API' do
 
       %w(id title body created_at updated_at).each do |attr|
         it "question object contains #{attr}" do
-          question = questions.first
           expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
+      end
+
+      context 'answers' do
+        it 'included in question object' do
+          expect(response.body).to have_json_size(1).at_path("0/answers")
+        end
+
+        %w(id body created_at updated_at).each do |attr|
+          it "contains #{attr}" do
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+          end
         end
       end
     end
