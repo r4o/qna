b73f4d961953640a6389846f67d10d6496e37ab8
r4o <r4o7r9@gmail.com>
2020-04-13 13:45:14 +0300

    Lesson_15_13

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -27,23 +27,23 @@ describe 'Questions API' do
       end
 
       it 'returns list of questions' do
-        expect(response.body).to have_json_size(2)
+        expect(response.body).to have_json_size(2).at_path("questions")
       end
 
       %w(id title body created_at updated_at).each do |attr|
         it "question object contains #{attr}" do
-          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("questions/0/#{attr}")
         end
       end
 
       context 'answers' do
         it 'included in question object' do
-          expect(response.body).to have_json_size(1).at_path("0/answers")
+          expect(response.body).to have_json_size(1).at_path("questions/0/answers")
         end
 
         %w(id body created_at updated_at).each do |attr|
           it "contains #{attr}" do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{attr}")
           end
         end
       end
