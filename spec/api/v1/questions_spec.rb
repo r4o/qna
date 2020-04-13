222dde1644fa638e93ce6ee764969f5d804b8888
r4o <r4o7r9@gmail.com>
2020-04-13 11:23:13 +0300

    Lesson_15_2

---
+++ spec/api/v1/questions_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Questions API' do
+  describe 'GET /index' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/questions', format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/questions', format: :json, access_token:'1234'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:user) }
+
+      it 'returns 200 status code' do
+        get '/api/v1/questions', format: :json, access_token: access_token.token
+        expect(response).to be_success
+      end
+    end
+  end
+end
