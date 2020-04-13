c85f262638501ae924c4c65d5644c6fc3f092339
r4o <r4o7r9@gmail.com>
2020-04-13 11:42:34 +0300

    Lesson_15_6

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -16,10 +16,22 @@ describe 'Questions API' do
 
     context 'authorized' do
       let(:access_token) { create(:access_token) }
+      let(:questions) { create_list(:question, 2) }
 
       it 'returns 200 status code' do
         get '/api/v1/questions', format: :json, access_token: access_token.token
         expect(response).to be_success
+      end
+
+      it 'returns list of questions' do
+        expect(response.body).to have_json_size(2)
+      end
+
+      %w(id title body created_at updated_at).each do |attr|
+        it "question object contains #{attr}" do
+          question = questions.first
+          expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
       end
     end
   end
