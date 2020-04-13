91164a948cfe2cb25c2a1227d04951670486fb3b
r4o <r4o7r9@gmail.com>
2020-04-13 12:56:24 +0300

    Lesson_15_10

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
   def index
     @questions = Question.all
-    respond_with @questions
+    respond_with @questions.to_json(include: :answers)
   end
 end
