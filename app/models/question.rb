5b06064f4b12049e9624222ddb5ecab3e83eb7a1
r4o <r4o7r9@gmail.com>
2020-04-17 12:43:13 +0300

    Lesson_16_5

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,17 @@ class Question < ApplicationRecord
   has_many :answers
   has_many :attachments, as: :attachmentable
   has_many :comments, as: :commentable
+  belongs_to :user
 
   validates :title, :body, presence: true
 
   accepts_nested_attributes_for :attachments
+
+  after_create :calculate_reputation
+
+  private
+
+  def calculate_reputation
+
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   subject { build(:question) }
-  
+
   it { should have_many :answers }
   it { should have_many :attachments }
 
@@ -10,4 +10,17 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :body }
 
   it { should accept_nested_attributes_for :attachments }
+
+  its(:title) { should == "MyString" }
+
+  it 'should calculate reputation after creating' do
+    expect(subject).to receive(:calculate_reputation)
+    subject.save!
+  end
+
+  it 'should not calculate reputation after update' do
+    subject.save!
+    expect(subject).to_not receive(:calculate_reputation)
+    subject.update(title: '123')
+  end
 end
