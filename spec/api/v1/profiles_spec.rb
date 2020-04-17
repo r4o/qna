c80a3cde5bcc03b390a1ed07648ed295aa7598f8
r4o <r4o7r9@gmail.com>
2020-04-17 11:11:21 +0300

    Lesson_16_2

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 describe 'Profile API' do
   describe 'Resource Owner Profile' do
-    context 'unauthorized' do
-      it 'returns 401 status code if there is no access_token' do
-        get '/api/v1/profiles/me', format: :json
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status code if access_token is invalid' do
-        get '/api/v1/profiles/me', format: :json, access_token:'1234'
-        expect(response.status).to eq 401
-      end
-    end
+    it_behaves_like "API Authenticable"
 
     context 'authorized' do
       let(:me) { create(:user) }
