133288ae8427e84a451a17f73ff4868f2ab1ade5
r4o <r4o7r9@gmail.com>
2020-04-20 14:36:45 +0300

    Lesson_17_13

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web =>'/sidekiq'
+  
   use_doorkeeper
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
